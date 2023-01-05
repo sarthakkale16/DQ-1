351c8c942200f360cde4114b9861d4017c005c52
Sarthak Kale <83166658+sarthakkale16@users.noreply.github.com>
2023-01-05 12:03:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQ-The-File-Donor
+  git clone https://github.com/sarthakkale16/selina.git /selina
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+  git clone $UPSTREAM_REPO /selina
 fi
-cd /DQ-The-File-Donor
+cd /selina
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting selina...."
 python3 bot.py
